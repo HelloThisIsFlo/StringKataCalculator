85561a0acb6b148c838da4446fc5c6f49c54b8ff
Florian Kempenich <ShockN745@gmail.com>
2016-02-19 18:18:27 +0100

    Zero/Null/SingleNumber/MultipleNumberWithComa

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,6 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class StringCalculator {
 
 
+    public int add(String operation) {
+        if (operation == null || operation.isEmpty()) {
+            return 0;
+        } else {
+            return add_operationNotEmpty(operation);
+        }
+    }
+
+    private int add_operationNotEmpty(String operation) {
+        String[] fragments = operation.split(",");
+        List<Integer> parsedInts = new ArrayList<>(fragments.length);
+        for (String fragment: fragments) {
+            parsedInts.add(Integer.parseInt(fragment));
+        }
+
+        return makeSum(parsedInts);
+    }
+
+    private int makeSum(List<Integer> numbers) {
+        int result = 0;
+        for (int i: numbers) {
+            result += i;
+        }
+        return result;
+    }
 
 
 }
--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -12,5 +12,30 @@ public class StringCalculatorTest {
         calculator = new StringCalculator();
     }
 
+    @Test
+    public void emptyString_returnZero() throws Exception {
+        String empty = "";
+        int result = calculator.add(empty);
+        assertEquals(0, result);
+    }
 
+    @Test
+    public void nullString_returnZero() throws Exception {
+        int result = calculator.add(null);
+        assertEquals(0, result);
+    }
+
+    @Test
+    public void singleNumber_returnNumber() throws Exception {
+        String singleNumber = "45";
+        int result = calculator.add(singleNumber);
+        assertEquals(45, result);
+    }
+
+    @Test
+    public void multipleNumberSeparaterComa_returnNumber() throws Exception {
+        String multipleNumbersComa = "324,4554,12";
+        int result = calculator.add(multipleNumbersComa);
+        assertEquals(324 + 4554 + 12, result);
+    }
 }
